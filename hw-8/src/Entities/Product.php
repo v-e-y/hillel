10f4f9abb2341b67e1aedffbad49bc138e577810
V-E-Y <v.eduard.y@gmail.com>
2022-04-09 13:27:32 +0300

    initialization hw-8

---
+++ hw-8/src/Entities/Product.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Hillel\Entities;
+
+use Hillel\Casts\ArrayCast;
+use Hillel\Casts\MoneyCast;
+use Hillel\Casts\DateTimeCast;
+
+class Product
+{
+    private float $price;
+
+    private string $attributes;
+
+    private int $updatedAt;
+
+    protected $casts = [
+        'price' => MoneyCast::class,
+        'attributes' => ArrayCast::class,
+        'updatedAt' => DateTimeCast::class,
+    ];
+
+    public function __construct($price, $attributes, $updatedAt)
+    {
+        $this->price = $price;
+        $this->attributes = $attributes;
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function __set($variable, $value)
+    {
+        // @todo
+    }
+
+    public function __get($variable)
+    {
+        // @todo
+    }
+
+    public function __toString(): string
+    {
+        // @todo
+
+        return '';
+    }
+}
